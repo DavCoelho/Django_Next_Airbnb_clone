d7c9703f24d93f73f7ef8c77c912d95ef8649ef8
David Coelho <coelho109.bcl@gmail.com>
2026-08-05 01:48:45 +0100

    creation of the property model, serializers for property, setting of the urls, and showing data fetched on the frontend, etc

---
+++ djangobnb/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
+      },
+    ],
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
