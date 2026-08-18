20198e24605a5c503291a02473369ffd1752b4d6
David Coelho <coelho109.bcl@gmail.com>
2026-08-18 14:15:29 +0000

    Update file settings.py

--- backend/djangobnb_backend/djangobnb_backend/settings.py
+++ backend/djangobnb_backend/djangobnb_backend/settings.py
@@ -208,3 +208,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
