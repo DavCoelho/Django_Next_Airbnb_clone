60f54a6c93ae6e4519e4a88f2538daf4f1ae5e86
David Coelho <coelho109.bcl@gmail.com>
2026-08-18 11:25:07 +0000

    still testing

--- backend/djangobnb_backend/djangobnb_backend/settings.py
+++ backend/djangobnb_backend/djangobnb_backend/settings.py
@@ -115,6 +115,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangobnb_backend.urls'
--- backend/djangobnb_backend/entrypoint.sh
+++ backend/djangobnb_backend/entrypoint.sh
@@ -13,4 +13,6 @@ fi
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 exec "$@"
